7601ff91807c73c6f094f456f55a8a1b55dd921d
dperco <danielperco4@gmail.com>
2024-05-18 13:46:23 -0300

    ok

---
+++ Clase 1 - Sintáxis y Variables/clase01/src/main/java/claseVariables.java
@@ -0,0 +1,20 @@
+public class claseVariables {
+
+    public static void main(String[] args){
+        int numero_prueba= 10;
+        float valorFloat= 123.456f;
+
+        float result;
+
+        String stringLarge="hola como ";
+        String charsuma="estan";
+        String resultString= stringLarge+charsuma;
+
+        result = numero_prueba + valorFloat;
+
+
+        System.out.println("mi numero es : " + result);
+        System.out.println("el string final es " + resultString);
+
+    }
+}
